47374296dcc130697c0e654baca70d7338546c59
Arnold Lesikar <alesikar@mac.com>
2015-05-22 18:31:12 -0500

    Revert "Debugging Pages"
    This reverts commit e22ec60e09025e0f04ea9cf725e44eac034def68.

--- plugins/submitByMailPlugin/confirmMsg.php
+++ plugins/submitByMailPlugin/confirmMsg.php
@@ -18,29 +18,26 @@ if (isset($_GET['mtk'])) {
 		$sbm->alids =unserialize($msgdata['listsaddressed']);
 		$fn = $sbm->escrowdir . $msgdata['file_name'];
 		$msg = file_get_contents($sbm->escrowdir . $msgdata['file_name']);
-		
-		print('<div style="font-size:14px;width:700px; margin-top:30px;">');
-		if ((count($sbm->alids) == 1) && ($doqueue = $sbm->doQueueMsg ($sbm->lid))) {
+		if ((count($sbm->alids) == 1) && ($doqueue = $sbm->doQueueMsg ($this->lid))) {
 			if ($qerr = $sbm->queueMsg($msg)) {
-				$msgid = $sbm->saveDraft($msg);
-				print('<p>Your message with the subject \'' . $sbm->subj . 
-								"' was not queued because of the following error(s):<br \> $qerr"
-								. "</p><p>The message has been saved as a draft.</p>");
-				print ('<p>'. $sbm->outsideLinkButton("send&id=$msgid", 'Edit Message') .'</p>');
-				logEvent("A message with the subject '" . $sbm->subj ."' received but not queued because of a problem.");
+				$sbm->mid = $sbm->saveDraft($msg);
+				print('<div style="font-size:14px;margin-top:30px;"><p>Your message with the subject \'' . $this->subj . 
+								"' was not queued because of the following error(s): $err"
+								. "</p><p>The message has been saved as a draft.</p></div>");
+				print ('<p>'. $sbm->outsideLinkButton("send&id={$sbm->mid}", 'Edit Message') .'</p>');
+				logEvent("A message with the subject '" . $this->subj ."' received but not queued because of a problem.");
 			} else {
-				print('<p>Your message with the subject \'' . $sbm->subj . "' was received and has been queued for distribution.</p>");
-				logEvent("A message with the subject '" . $sbm->subj ."' was received and queued.");
+				print('<div style="font-size:14px;margin-top:30px;"><p>Your message with the subject \'' . $this->subj . "' was received and has been queued for distribution.</p></div>");
+				logEvent("A message with the subject '" . $this->subj ."' was received and queued.");
 			}
-		} else {	
-			$msgid = $sbm->saveDraft($msg);
-			print ('<p>Your message with the subject \'' . $sbm->subj . "' was received and has been saved as a draft.</p>");
-			print ('<p>'. $sbm->outsideLinkButton("send&id=$msgid", 'Edit Message') .'</p>');
-			logEvent("A message with the subject '" . $sbm->subj ."' was received and and saved as a draft.");
-		}	
-		print('</div>');		
+		} else {		
+			$sbm->mid = $this->saveDraft($msg);
+			print ('<p>Your message with the subject \'' . $this->subj . "' was received and has been saved as a draft.</p>");
+			print ('<p>'. $sbm->outsideLinkButton("send&id={$sbm->mid}", 'Edit Message') .'</p>');
+			logEvent("A message with the subject '" . $this->subj ."' was received and and saved as a draft.");
+		}			
 		unlink($fn);
-		$query = sprintf ("delete from %s where token = '%s'", $sbm->tables['escrow'], $token);
+		$query = sprintf ("delete from %s where token = '%s'", $this->tables['escrow'], $token);
     	Sql_Query($query);
 	} else
 		print ('<div style="font-size:14px !important;margin-top:30px;color:red !important"><p>Message not found.</p><p>You either have a typo in the URL or the hold time for the message has expired.</p></div>');
--- plugins/submitByMailPlugin/pipeInMsg.php
+++ plugins/submitByMailPlugin/pipeInMsg.php
@@ -28,29 +28,26 @@
  * 
  */
  
-if (!defined('PHPLISTINIT')) die(); ## avoid pages being loaded directly
-ob_end_clean();
-$sbm = $GLOBALS['plugins']['submitByMailPlugin'];
-$mbox = $cline['e'];
-if (!$mbox) {
- 	echo "No mailbox specified.\n";
+ if (!defined('PHPLISTINIT')) die(); ## avoid pages being loaded directly
+ $sbm = $GLOBALS['plugins']['submitByMailPlugin'];
+ if (!($mbox = $cline['e'])) {
  	logEvent("Message discarded: no mailbox specified with pipe");
  	die();
-}
-if (!($mylid = $sbm->getListId($mbox))) {
- 	echo "No list associated with this pipe.\n";
- 	logEvent("Message discarded: no list associated with mailbox using this pipe");
+ }
+ if (!($sbm->getListId($mbox))) {
+ 	logEvent("Message discarded: no list associated with mailbox with pipe");
  	die();
-}
-if (!$sbm->pipeOK($mylid)) {
- 	echo "Pipe submission not permitted for this list.\n";
- 	logEvent("Message discarded: list $mylid not assigned to pipe submission.");
+ }
+ $query = sprintf ("select pipe_submission from %s where id=%d", $this->tables['list'], $this->lid);
+ $row = Sql_Fetch_Array_Query($query);
+ if (!$row[0]) {
+ 	logEvent("Message discarded: list " . $this->lid . " not assigned to pipe submission.");
  	die();
-}
-$msg = file_get_contents('php://stdin');
-// Process the message
-$sbm->receiveMsg($msg, $mbox);
-echo "Message processed. Check Event Log for result.\n"; */
+  }
+ ob_end_clean();
+ $msg = file_get_contents('php://stdin');
+ // Process the message
+ $sbm->receiveMsg($msg, $mbox);
 ?>
  
  
